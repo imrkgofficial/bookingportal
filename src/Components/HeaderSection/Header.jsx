067aff7dc49ba444538ae7c3c474c9d66c559dbf
“SIYARAM <siyaramsaini2018@gmail.com>
2025-02-05 14:14:54 +0530

    solved error

--- src/Components/HeaderSection/Header.jsx
+++ src/Components/HeaderSection/Header.jsx
@@ -5,13 +5,9 @@ import { IoIosArrowDown } from "react-icons/io";
 const Header = () => {
   return (
     <div className="top-0 sticky">
-      <div className="bg-slate-100 shadow-sm relative">
-        <img
-          src={logo}
-          alt="Logo"
-          className="absolute top-2 ml-[3rem] sm:ml-5 md:ml-10"
-        />
-        <ul className="flex justify-center gap-[4rem] h-[4rem] items-center sm:flex-col sm:gap-4 sm:h-auto sm:pt-16 md:gap-[6rem]">
+      <div className="bg-slate-100 shadow-sm">
+        <img src={logo} alt="" className="absolute top-2 ml-[3rem]" />
+        <ul className="flex justify-center gap-[4rem] h-[4rem] items-center">
           <div className="flex items-center gap-2 text-gray-400">
             <li className="text-[16px] text-gray-400 cursor-pointer">Home</li>
             <IoIosArrowDown className="cursor-pointer" />
@@ -30,11 +26,10 @@ const Header = () => {
           </div>
         </ul>
       </div>
-      <button className="absolute ml-[70rem] top-1 p-3 w-[12rem] bg-blue-600 rounded-xl border font-medium text-white sm:ml-[50%] sm:transform sm:-translate-x-[50%] sm:w-[80%] sm:mt-4 md:w-[15rem] lg:w-[12rem]">
+      <button className="absolute ml-[70rem] top-1 p-3 w-[12rem] bg-blue-600 rounded-xl border font-medium text-white">
         Free Consultation
       </button>
     </div>
   );
 };
-
 export default Header;
